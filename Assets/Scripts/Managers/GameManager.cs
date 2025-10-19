c6c15514dccb0966dae61ef10137f352943d283a
yonatan <tanims41@gmail.com>
2025-10-19 14:08:52 +0300

    game over screen

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,13 +2,35 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public float gravityModerator = 2.5f;
+    public GameObject gameOverScreen;
+    public float gravityModerator = 1f;
     public static float ScreenBufferX = 0.05f;
+    public static GameManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
 
     void Start()
     {
         Physics.gravity *= gravityModerator;
-
+        PlayerController.onScissorsCatch += OnGameOver;
     }
 
+    private void OnGameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+
+    private void OnDestroy()
+    {
+        if (instance == this)
+            instance = null;
+    }
 }
